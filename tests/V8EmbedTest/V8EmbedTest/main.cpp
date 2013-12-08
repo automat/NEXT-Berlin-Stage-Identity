35959756ae7ced6d611d79ba0fcf95f871a4a576
Henryk Wollik <olafyoorsen@hotmail.com>
2013-12-08 22:15:52 +0100

    added V8EmbedTest

---
+++ tests/V8EmbedTest/V8EmbedTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  V8EmbedTest
+//
+//  Created by Henryk Wollik on 08/12/13.
+//  Copyright (c) 2013 atm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
