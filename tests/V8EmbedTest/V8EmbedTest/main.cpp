38ea6b8e179424686e05167e2833dafc4d987a29
Henryk Wollik <olafyoorsen@hotmail.com>
2013-12-09 00:17:17 +0100

    added initial v8 cpp test

--- tests/V8EmbedTest/V8EmbedTest/main.cpp
+++ tests/V8EmbedTest/V8EmbedTest/main.cpp
@@ -7,12 +7,35 @@
 //
 
 #include <iostream>
+#include <v8.h>
+
+
+
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    v8::Isolate* isolate = v8::Isolate::GetCurrent();
+    
+    v8::HandleScope handle_scope(isolate);
+    
+    v8::Handle<v8::Context> context = v8::Context::New(isolate);
+    
+    v8::Persistent<v8::Context> persistent_context(isolate,context);
+    
+    v8::Context::Scope contex_scope(context);
+    
+    v8::Handle<v8::String> source = v8::String::New("'Hello ' + 'Darth Vader'");
+    
+    v8::Handle<v8::Script> script = v8::Script::Compile(source);
+    
+    v8::Handle<v8::Value> result = script->Run();
+    
+    persistent_context.Dispose();
+    
+    v8::String::AsciiValue ascii(result);
+    
+    printf("%s\n", *ascii);
+    
+     return 0;
 }
 
