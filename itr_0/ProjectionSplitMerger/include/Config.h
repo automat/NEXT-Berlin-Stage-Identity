d602a8fb9cfea4e6010dad61a3f394ad5d92e446
Henryk Wollik <olafyoorsen@hotmail.com>
2014-03-11 17:43:38 +0100

    working gooood

--- itr_0/ProjectionSplitMerger/include/Config.h
+++ itr_0/ProjectionSplitMerger/include/Config.h
@@ -24,28 +24,43 @@
 #define APP_FPS 32.0f
 */
 
-#define DISPLAY_WIDTH  1920
+//#define DISPLAY_WIDTH  1920
+
+#include "cinder/Area.h"
+#include "cinder/Rect.h"
+#include "cinder/gl/TextureFont.h"
+
+using namespace ci;
 
 static int   PROJECTION_EDGE_WIDTH, PROJECTION_EDGE_WEIGHT;
 
+static int   WINDOW_PROJECTION_SCALE;
 static int   WINDOW_WIDTH, WINDOW_HEIGHT;
 static bool  WINDOW_REZISABLE, WINDOW_BORDERLESS;
 static int   WINDOW_DISPLAY;
 static bool  WINDOW_FIXED_POSITION;
+static bool  WINDOW_ALWAYS_ON_TOP;
 static int   WINDOW_POSITION_X, WINDOW_POSITION_Y;
+
+static int   DISPLAY_WIDTH;
 
 static float APP_FPS;
 static int   APP_VIEWPORT_WIDTH, APP_VIEWPORT_HEIGHT;
 static bool  APP_DEBUG_DRAW_VIEWPORTS;
 static int   APP_ANTIALIASING;
 
+static Area  FBO_DISPLAY0_AREA;
+static Area  FBO_DISPLAY1_AREA;
+
+static Rectf DISPLAY0_RECT;
+static Rectf DISPLAY1_RECT;
 
 #include <iostream>
 #include <string>
 #include "cinder/Json.h"
 
+
 namespace config {
-    using namespace ci;
     using namespace std;
     
     inline static void Load(const string& path){
@@ -54,24 +69,39 @@ namespace config {
         // Projection
         int projectionWidth  = json.getChild("Projection.Width").getValue<int>();
         int projectionHeight = json.getChild("Projection.Height").getValue<int>();
+        
+        
+        // Window
+        WINDOW_PROJECTION_SCALE = json.getChild("Window.ProjectionScale").getValue<int>();
+        WINDOW_WIDTH            = projectionWidth;
+        WINDOW_HEIGHT           = projectionHeight;
+        WINDOW_REZISABLE        = json.getChild("Window.Rezisable").getValue<bool>();
+        WINDOW_BORDERLESS       = json.getChild("Window.Borderless").getValue<bool>();
+        WINDOW_FIXED_POSITION   = json.getChild("Window.FixedPosition").getValue<bool>();
+        WINDOW_ALWAYS_ON_TOP    = json.getChild("Window.AlwaysOnTop").getValue<bool>();
+        WINDOW_POSITION_X       = json.getChild("Window.Position.x").getValue<int>();
+        WINDOW_POSITION_Y       = json.getChild("Window.Position.y").getValue<int>();
+        
         PROJECTION_EDGE_WIDTH  = json.getChild("Projection.Edge.Width").getValue<int>();
         PROJECTION_EDGE_WEIGHT = json.getChild("Projection.Edge.Weight").getValue<int>();
         
-        // Window
-        int windowProjectionScale = json.getChild("Window.ProjectionScale").getValue<int>();
-        WINDOW_WIDTH          = projectionWidth  / windowProjectionScale;
-        WINDOW_HEIGHT         = projectionHeight / windowProjectionScale;
-        WINDOW_REZISABLE      = json.getChild("Window.Rezisable").getValue<bool>();
-        WINDOW_BORDERLESS     = json.getChild("Window.Borderless").getValue<bool>();
-        WINDOW_FIXED_POSITION = json.getChild("Window.FixedPosition").getValue<bool>();
-        WINDOW_POSITION_X     = json.getChild("Window.Position.x").getValue<int>();
-        WINDOW_POSITION_Y     = json.getChild("Window.Position.y").getValue<int>();
+        DISPLAY_WIDTH = ( projectionWidth + PROJECTION_EDGE_WIDTH ) / 2;
+        
         
         // App
         APP_FPS = json.getChild("App.FPS").getValue<float>();
-        APP_VIEWPORT_WIDTH  = WINDOW_WIDTH - PROJECTION_EDGE_WIDTH / windowProjectionScale;
+        APP_VIEWPORT_WIDTH  = WINDOW_WIDTH - PROJECTION_EDGE_WIDTH ;
         APP_VIEWPORT_HEIGHT = WINDOW_HEIGHT;
         APP_ANTIALIASING    = json.getChild("App.Antialiasing").getValue<int>();
+        
+        DISPLAY0_RECT = Rectf(0,0,DISPLAY_WIDTH,WINDOW_HEIGHT);
+        DISPLAY1_RECT = Rectf(DISPLAY_WIDTH,0,DISPLAY_WIDTH + DISPLAY_WIDTH - PROJECTION_EDGE_WIDTH, WINDOW_HEIGHT);
+        
+        FBO_DISPLAY0_AREA = Area(Vec2f(),
+                                 Vec2f(DISPLAY_WIDTH, APP_VIEWPORT_HEIGHT));
+        FBO_DISPLAY1_AREA = Area(Vec2f(DISPLAY_WIDTH - PROJECTION_EDGE_WIDTH,0),
+                                 Vec2f(DISPLAY_WIDTH - PROJECTION_EDGE_WIDTH + APP_VIEWPORT_WIDTH / 2, APP_VIEWPORT_HEIGHT));
+    
     }
 }
 
--- itr_0/ProjectionSplitMerger/src/ProjectionSplitMergerApp.cpp
+++ itr_0/ProjectionSplitMerger/src/ProjectionSplitMergerApp.cpp
@@ -3,6 +3,8 @@
 #include "config.h"
 #include "cinder/gl/Fbo.h"
 #include "cinder/Camera.h"
+#include "cinder/gl/TextureFont.h"
+#include "cinder/Utilities.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -29,6 +31,9 @@ class ProjectionSplitMergerApp : public AppNative {
 
     void drawScene();
 	void draw();
+    void drawStringWithBg(gl::TextureFontRef font, const string& str, const Vec2f& center);
+    
+    gl::TextureFontRef mDebugTexFont60;
     
     CameraOrtho mCamera;
     CameraPersp mCameraPersp;
@@ -50,7 +55,7 @@ void ProjectionSplitMergerApp::prepareSettings(Settings* settings){
     if (WINDOW_FIXED_POSITION) {
         settings->setWindowPos(WINDOW_POSITION_X, WINDOW_POSITION_Y);
     }
-    settings->setAlwaysOnTop(true);
+    settings->setAlwaysOnTop(WINDOW_ALWAYS_ON_TOP);
     
     // Set settings
     settings->setWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -58,8 +63,10 @@ void ProjectionSplitMergerApp::prepareSettings(Settings* settings){
 }
 
 void ProjectionSplitMergerApp::setup(){
+    mDebugTexFont60 = gl::TextureFont::create(Font("Apercu Mono", 40));
+    
     float aspectRatio = float(APP_VIEWPORT_WIDTH) / float(APP_VIEWPORT_HEIGHT);//app::getWindowAspectRatio();
-    mCamera.setOrtho(-aspectRatio, aspectRatio, -1, 1, -10.0f, 1);
+    mCamera.setOrtho(-aspectRatio, aspectRatio, -1, 1, -10.0f, 10.0f);
     mCamera.lookAt(Vec3f(-1,1,-1), Vec3f::zero());
 
     gl::Fbo::Format fboFormat;
@@ -71,6 +78,7 @@ void ProjectionSplitMergerApp::setup(){
 }
 
 void ProjectionSplitMergerApp::drawScene(){
+    // Sample scene
     mFbo.bindFramebuffer();
     glPushAttrib(GL_VIEWPORT_BIT),
     glEnable(GL_DEPTH_TEST);
@@ -81,13 +89,18 @@ void ProjectionSplitMergerApp::drawScene(){
     
     static const Vec3f zero;
     static const Vec3f unit(1,1,1);
-    
+    int num    = 10;
+    float step = float(M_PI * 2) / float(num) ;
     glPushMatrix();
-    glScalef(3,3,3);
-    glRotatef(app::getElapsedSeconds()*20, 0, 1, 0);
-    gl::drawColorCube(zero, unit);
+    glRotatef(app::getElapsedSeconds() * 100, 0, 1, 0);
+    for(int i = 0; i < num; ++i){
+        glPushMatrix();
+        glTranslatef(cosf(step *  float(i) ), 0, sin(step * float(i) ));
+        gl::drawColorCube(zero, unit * 0.125f);
+        glPopMatrix();
+        
+    }
     glPopMatrix();
-    
     
     glDisable(GL_DEPTH_TEST);
     glPopAttrib();
@@ -106,45 +119,88 @@ void ProjectionSplitMergerApp::update(){
     
 }
 
+void ProjectionSplitMergerApp::drawStringWithBg(gl::TextureFontRef font, const string& str, const Vec2f& center){
+    float fontDescent    = font->getDescent();
+    Vec2f fontStringSize = font->measureString(str);
+    Rectf stringRect     = Rectf(center.x - fontStringSize.x * 0.5f,
+                                 center.y - fontStringSize.y * 0.5f,
+                                 center.x + fontStringSize.x * 0.5f,
+                                 center.y + fontStringSize.y * 0.5f);
+    
+    
+    glPushMatrix();
+    glColor3f(0, 0, 0);
+    gl::drawSolidRect(stringRect);
+    glTranslatef(stringRect.x1, stringRect.y1, 0);
+    glScalef( 1, -1, 1);
+    glColor3f(1, 1, 1);
+    font->drawString(str, Vec2f(0,-fontDescent));
+    glPopMatrix();
+}
 
 void ProjectionSplitMergerApp::draw(){
+
+
     drawScene();
     
     gl::clear(Color(0,0,0));
     gl::setMatricesWindow(app::getWindowSize(),false);
     
-    const static Area  fboArea0(Vec2f(0,0),Vec2f(APP_VIEWPORT_WIDTH / 2, APP_VIEWPORT_HEIGHT));
-    const static Area  fboArea1(Vec2f(APP_VIEWPORT_WIDTH / 2 - PROJECTION_EDGE_WIDTH,0),Vec2f(APP_VIEWPORT_WIDTH / 2 - PROJECTION_EDGE_WIDTH + APP_VIEWPORT_WIDTH / 2, APP_VIEWPORT_HEIGHT));
-    
-    const static Rectf display0(0,0,DISPLAY_WIDTH, WINDOW_HEIGHT);
-    const static Rectf display1(DISPLAY_WIDTH,0,DISPLAY_WIDTH + DISPLAY_WIDTH, WINDOW_HEIGHT);
     
     //draw first screen
     glColor3f(1,1,1);
-    gl::draw(mFbo.getTexture(),fboArea0,display0);
-    gl::draw(mFbo.getTexture(),fboArea1,display1);
-    
-    glColor3f(1, 1, 1);
+    gl::draw(mFbo.getTexture(),FBO_DISPLAY0_AREA,DISPLAY0_RECT);
     glPushMatrix();
-    glTranslatef(20, 20, 0);
-    glScalef(-1, 0, 0);
-    gl::drawString("DISPLAY 0",Vec2f::zero());
+    //glTranslatef(-(0.5f + sinf(app::getElapsedSeconds()) * 0.5f) * float(PROJECTION_EDGE_WIDTH), 0, 0);
+    gl::draw(mFbo.getTexture(),FBO_DISPLAY1_AREA,DISPLAY1_RECT);
     glPopMatrix();
+    
+    // debug view
+    
+    //return;
+    
+    glLineWidth(10);
     glColor3f(0, 1, 0);
-    gl::drawStrokedRect(display0); // draw area display0
-    gl::drawStrokedRect(display1); // draw area display1
+    gl::drawStrokedRect(DISPLAY0_RECT); // draw area display0
+    glLineWidth(1);
     glEnable(GL_ALPHA_TEST);
     gl::enableAlphaBlending();
+    
+    glPushMatrix();
+    //glTranslatef(-(0.5f + sinf(app::getElapsedSeconds()) * 0.5f) * float(PROJECTION_EDGE_WIDTH), 0, 0);
     glColor3f(1,0,0);
     gl::drawStrokedRect(Rectf(DISPLAY_WIDTH,0,
                               DISPLAY_WIDTH + PROJECTION_EDGE_WIDTH, APP_VIEWPORT_HEIGHT));
     glColor4f(0, 0, 0, 0.75f);
     gl::drawSolidRect(Rectf(DISPLAY_WIDTH, 0,
                             DISPLAY_WIDTH + PROJECTION_EDGE_WIDTH, APP_VIEWPORT_HEIGHT));
+    drawStringWithBg(mDebugTexFont60, "Edge: " + toString(PROJECTION_EDGE_WIDTH), Vec2f(DISPLAY_WIDTH + PROJECTION_EDGE_WIDTH * 0.5f,APP_VIEWPORT_HEIGHT * 0.5f));
+    glLineWidth(10);
+    glColor3f(0, 1, 0);
+    gl::drawStrokedRect(DISPLAY1_RECT); // draw area display1
+    glLineWidth(1);
+
+    glPopMatrix();
+    
+    glColor3f(1, 1, 1);
+    
+
+    static const string display0String     = "Display 0: " + toString(DISPLAY_WIDTH) + " x " + toString(WINDOW_HEIGHT) ;
+    static const string display1String     = "Display 1: " + toString(DISPLAY_WIDTH) + " x " + toString(WINDOW_HEIGHT) ;
+    static const Vec2f  display0Center     = Vec2f(DISPLAY_WIDTH * 0.5f, APP_VIEWPORT_HEIGHT * 0.5f);
+    static const Vec2f  display1Center     = Vec2f(DISPLAY_WIDTH + DISPLAY_WIDTH * 0.5f, display0Center.y);
+    
+    drawStringWithBg(mDebugTexFont60, display0String, display0Center);
+    drawStringWithBg(mDebugTexFont60, "Viewport 0: " + toString(DISPLAY_WIDTH) + " x " + toString(APP_VIEWPORT_HEIGHT), Vec2f(display0Center.x,display0Center.y - 80));
+    drawStringWithBg(mDebugTexFont60, display1String, display1Center);
+    drawStringWithBg(mDebugTexFont60, "Viewport 1: " + toString(DISPLAY_WIDTH - PROJECTION_EDGE_WIDTH) + " x " + toString(APP_VIEWPORT_HEIGHT), Vec2f(display1Center.x,display1Center.y - 80));
+    
     
     gl::disableAlphaBlending();
     glDisable(GL_ALPHA_TEST);
     //draw second screen
+    
+
     
     
 }
