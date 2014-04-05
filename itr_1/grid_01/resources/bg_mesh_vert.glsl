5070b312b1b491f3df4c179c84369a84d7acdc25
Henryk Wollik <olafyoorsen@hotmail.com>
2014-04-05 21:19:47 +0200

    ...

--- itr_1/grid_01/resources/bg_mesh_vert.glsl
+++
@@ -1,8 +0,0 @@
-varying vec3 vNormal;
-varying vec3 vVertex;
-
-void main(){
-    vVertex = vec3(gl_ModelViewMatrix * gl_Vertex);       
-	vNormal = normalize(gl_NormalMatrix * gl_Normal);
-    gl_Position = ftransform();
-}
--- itr_1/grid_01/resources/bg_mix_frag.glsl
+++
@@ -1,7 +0,0 @@
-uniform sampler2D uGradientSampler;
-uniform sampler2D uMeshSampler;
-
-void main(){
-	
-    gl_FragColor = vec4(1,1,1,1);
-}
