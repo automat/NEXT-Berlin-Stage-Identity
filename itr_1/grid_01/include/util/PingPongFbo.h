c13b014b5b9d27bf6327a36b3ed0ab74e9e67a32
Henryk Wollik <olafyoorsen@hotmail.com>
2014-04-07 00:10:14 +0200

    pingpong fbos now based on color attachment

--- itr_1/grid_01/include/util/PingPongFbo.h
+++ itr_1/grid_01/include/util/PingPongFbo.h
@@ -13,27 +13,40 @@
 
 using namespace ci;
 
-class PingPongFbo {
+class PingPongFbo : public gl::Fbo{
 private:
-    gl::Fbo mFbo[2];
+    GLenum  mBuffers[2];
     int		mCurrentIndex;
 
 public:
-    PingPongFbo(){}
+    PingPongFbo() : gl::Fbo(){}
     
-    PingPongFbo(int width, int height, gl::Fbo::Format format = gl::Fbo::Format()) {
-        mFbo[0] = gl::Fbo(width, height, format);
-        mFbo[1] = gl::Fbo(width, height, format);
-        
+    PingPongFbo(int width, int height, gl::Fbo::Format format = gl::Fbo::Format()) :
+    gl::Fbo(width, height, format){
+        mBuffers[0] = GL_COLOR_ATTACHMENT0;
+        mBuffers[1] = GL_COLOR_ATTACHMENT1;
         mCurrentIndex = 0;
     }
     
-    inline gl::Fbo& getTarget() {
-        return mFbo[mCurrentIndex];
+    inline void bindFramebuffer(){
+        gl::Fbo::bindFramebuffer();
+        glDrawBuffers(2, mBuffers);
     }
     
-    inline gl::Fbo& getSource() {
-        return mFbo[1-mCurrentIndex];
+    inline GLenum getTarget() {
+        return mBuffers[mCurrentIndex];
+    }
+    
+    inline GLenum getSource() {
+        return mBuffers[1-mCurrentIndex];
+    }
+    
+    inline gl::Texture& getTargetTexture(){
+        return getTexture(getTarget());
+    }
+    
+    inline gl::Texture& getSourceTexture(){
+        return getTexture(getSource());
     }
     
     void swap() {
