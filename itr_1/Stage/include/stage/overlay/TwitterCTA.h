9fa4a22b9dc39b72b87c97a0cdc8da999ec04d0e
Henryk Wollik <olafyoorsen@hotmail.com>
2014-05-02 21:37:50 +0200

    twitter initial

---
+++ itr_1/Stage/include/stage/overlay/TwitterCTA.h
@@ -0,0 +1,27 @@
+//
+//  TwitterCTA.h
+//  Stage
+//
+//  Created by Henryk Wollik on 02/05/14.
+//
+//
+
+#ifndef Stage_TwitterCTA_h
+#define Stage_TwitterCTA_h
+
+#include "util/text/TextBox.h"
+#include "util/text/TextBoxTexture.h"
+
+namespace next {
+    class TwitterCTA {
+        
+        
+    public:
+        
+    };
+}
+
+
+
+
+#endif
