07267ed6fc346010256476e03cf18af0e0084919
Henryk Wollik <olafyoorsen@hotmail.com>
2014-05-03 12:49:51 +0200

    added twitter call to action defines

--- itr_1/Stage/include/stage/overlay/TwitterCTA.h
+++ itr_1/Stage/include/stage/overlay/TwitterCTA.h
@@ -29,7 +29,9 @@ namespace next {
         TextBoxTexture mText1;
         TextBoxTexture mText2;
         gl::Texture    mImage;
+        
         Matrix44f      mTransform;
+        
         
         Anim<float>    mAlphaState;
         
@@ -41,26 +43,26 @@ namespace next {
             mAlphaState(0.0f){
             mImage = gl::Texture(loadImage(app::loadResource(RES_IMAGE_TWITTER)));
             mTransform.setToIdentity();
-            mTransform.translate(Vec3f(TWITTER_LOGO_MARGIN_LEFT, STAGE_HEIGHT - TWITTER_LOGO_MARGIN_BOTTOM, 0));
+            mTransform.translate(Vec3f(TWITTER_CTA_LOGO_MARGIN_LEFT, STAGE_HEIGHT - TWITTER_CTA_LOGO_MARGIN_BOTTOM, 0));
             
             TextBox textbox;
-            textbox.setFont(Font(app::loadResource(RES_AKKURAT_BOLD),38 * 8));
+            textbox.setFont(Font(app::loadResource(RES_AKKURAT_BOLD),TWITTER_CTA_FONT_SIZE * TWITTER_CTA_FONT_SCALAR));
             textbox.setFontSize(38);
-            textbox.setColorFont(Color::white());
+            textbox.setColorFont(TWITTER_CTA_FONT_COLOR_0);
             textbox.setWidth(800);
             
-            textbox.setString("What is your New Normal?");
+            textbox.setString(TWITTER_CTA_TEXT_STRING_0);
             mText0.topleft = textbox.getTopLeft();
             mText0.calculatedSize = textbox.getCalculatedSize();
             mText0.texture = gl::Texture(Surface(textbox.getTexture()));
             
-            textbox.setString("Selected tweets from");
+            textbox.setString(TWITTER_CTA_TEXT_STRING_1);
             mText1.topleft = textbox.getTopLeft();
             mText1.calculatedSize = textbox.getCalculatedSize();
             mText1.texture = gl::Texture(Surface(textbox.getTexture()));
             
-            textbox.setColorFont(COLOR_NEXT_BLUE);
-            textbox.setString("#newnormal #next14");
+            textbox.setColorFont(TWITTER_CTA_FONT_COLOR_1);
+            textbox.setString(TWITTER_CTA_TEXT_STRING_2);
             mText2.topleft = textbox.getTopLeft();
             mText2.calculatedSize = textbox.getCalculatedSize();
             mText2.texture = gl::Texture(Surface(textbox.getTexture()));
@@ -78,11 +80,11 @@ namespace next {
             glMultMatrixf(&mTransform[0]);
             glColor4f(1, 1, 1, alpha);
             gl::draw(mImage);
-            glTranslatef(TWITTER_LOGO_SIZE + 20, 20, 0);
+            glTranslatef(TWITTER_CTA_LOGO_SIZE + TWITTER_CTA_TEXT_MARGIN_LEFT, TWITTER_CTA_TEXT_MARGIN_RIGHT, 0);
             gl::draw(mText0.texture);
-            glTranslatef(0, 38 + 8, 0);
+            glTranslatef(0, TWITTER_CTA_FONT_SIZE + TWITTER_CTA_TEXT_MARGIN_TOP, 0);
             gl::draw(mText1.texture);
-            glTranslatef(mText1.calculatedSize.x + 5, 0, 0);
+            glTranslatef(mText1.calculatedSize.x + TWITTER_CTA_TEXT_HASHTAG_MARGIN_LEFT, 0, 0);
             gl::draw(mText2.texture);
             glPopMatrix();
         }
